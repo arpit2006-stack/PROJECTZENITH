cb0fba4cc7593c34dca2b4d931b86e1aeea3bdcb
arpit2006-stack <arpitsb20@gmail.com>
2025-02-27 18:32:28 +0530

    arrangement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,52 +7,31 @@ import BannerSlider from "./components/sliderOne";
 import Bncardstwo from "./components/bnCardsTwo";
 import OfferOne from "./components/offerOne";
 import Footer from "./components/footer";
-<<<<<<< HEAD
+
 import AuthForm from "./pages/signup";
-=======
+
 import BrandSlider from "./components/sliderTwo";
 import Home from "./components/home";
 
 
->>>>>>> df9d3c5e8c2d92f774a25defaefb416869bb3377
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <header>
         <Navbar />
       </header>
       <main>
-        
-        <Bncards />
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/signup" element={<AuthForm />} />
         </Routes>
       </main>
-      <BannerSlider />
-      <Bncardstwo />
-      <OfferOne />
       <footer>
         <Footer />
       </footer>
-=======
-   
-    <Navbar/>
-    <Home/>
-    {/* <BrOne/> */}
-    <main>
-     {/* <Bncards/> */}
-    </main>
-    {/* <BannerSlider/> */}
-    {/* <Bncardstwo/> */}
-    {/* <OfferOne/> */}
-    {/* <BrandSlider /> */}
-    <Footer/>
 
 
-    
->>>>>>> df9d3c5e8c2d92f774a25defaefb416869bb3377
     </>
   );
 }
